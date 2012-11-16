d21346d8fb82497deed985b70b5e357e6e86d6e4
Jan.Wang <janindataage@163.com>
2012-11-16 15:51:08 +0800

    add archetype application for module and application

---
+++ archetype-jan-app/src/main/resources/archetype-resources/src/main/java/HelloWorld.java
@@ -0,0 +1,15 @@
+package ${package};
+
+/**
+ * First Class.
+ * 
+ * @author Jan Wong
+ */
+public class HelloWorld {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+    
+}
