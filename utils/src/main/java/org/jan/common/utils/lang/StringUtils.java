d9ed69ab21c0f96f210f39fa75ed1d92220bf048
Jan.Wang <janindataage@163.com>
2012-12-05 16:53:21 +0800

    update StringUtils.java

--- utils/src/main/java/org/jan/common/utils/lang/StringUtils.java
+++ utils/src/main/java/org/jan/common/utils/lang/StringUtils.java
@@ -130,6 +130,14 @@ public class StringUtils {
      */
     private static final Pattern WHITESPACE_BLOCK = Pattern.compile("\\s+");
 
+    private static final char C1 = 'a';
+
+    private static final char C2 = 'z';
+
+    private static final char C3 = 'A';
+
+    private static final char C4 = 'Z';
+
     /**
      * <p>{@code StringUtils} instances should NOT be constructed in
      * standard programming. Instead, the class should be used as
@@ -6541,4 +6549,36 @@ public class StringUtils {
         return charsetName == null ? new String(bytes) : new String(bytes, charsetName);
     }
 
+    /**
+     *
+     * Shifts characters of a String according to digits.
+     *
+     * @param str
+     * @param digits
+     * @return a new String
+     */
+    public static String shift(String str, int digits){
+        digits %= 26;
+        if (digits == 0)
+            return str;
+        char[] chars = str.toCharArray();
+        for (int i = chars.length - 1; i >= 0; i--) {
+            if (chars[i] >= C3 && chars[i] <= C4) {
+                chars[i] += digits;
+                if (chars[i] > C4) {
+                    chars[i] = (char) (chars[i] - C4 + C3 - 1);
+                } else if (chars[i] < C3) {
+                    chars[i] = (char) (C4 - (C3 - chars[i]) + 1);
+                }
+            } else if (chars[i] >= C1 && chars[i] <= C2) {
+                chars[i] += digits;
+                if (chars[i] > C2) {
+                    chars[i] = (char) (chars[i] - C2 + C1 - 1);
+                } else if (chars[i] < C1) {
+                    chars[i] = (char) (C2 - (C1 - chars[i]) + 1);
+                }
+            }
+        }
+        return new String( chars );
+    }
 }
